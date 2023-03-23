9882785c5801708444f9776ea1cf837a57283c46
Samith Vinura <104480599+SamithVinura@users.noreply.github.com>
2023-03-23 11:13:55 +0530

    initialize all plugins

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'angular-password-manager-c5880',
+    appId: '1:314984442621:web:eddbd74fbba6343e91ec94',
+    storageBucket: 'angular-password-manager-c5880.appspot.com',
+    apiKey: 'AIzaSyAG2xFz09lCpw-3B6C7P0-t_MioohQGnzo',
+    authDomain: 'angular-password-manager-c5880.firebaseapp.com',
+    messagingSenderId: '314984442621',
+  },
+  production:false
+};
