d1a72252498be66e908d432884136af65b452b18
Samith Vinura <104480599+SamithVinura@users.noreply.github.com>
2023-03-27 10:37:31 +0530

    add new site to firebase function done

--- src/app/site-list/site-list.component.ts
+++ src/app/site-list/site-list.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { PasswordManagerService } from '../password-manager.service';
 
 @Component({
   selector: 'app-site-list',
   templateUrl: './site-list.component.html',
-  styleUrls: ['./site-list.component.css']
+  styleUrls: ['./site-list.component.css'],
 })
 export class SiteListComponent {
 
+  constructor(private passwordManager:PasswordManagerService){}
+  onSubmit(values: object) {
+    this.passwordManager.addSite(values)
+  }
 }
